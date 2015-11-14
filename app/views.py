62f0b24aad1b73665d315311fa1d14a2652b378c
iamericyuan <iamericyuan@outlook.com>
2015-11-14 16:36:52 +0800

    以Bootstrap为基础，搭建的html框架初稿

--- app/views.py
+++ app/views.py
@@ -7,33 +7,16 @@ from .forms import LoginForm
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname': 'Eric Yuan'}
-    blogs = [
-        {
-            'author': {'nickname': 'Eric Yuan'},
-            'content': 'This is the first blog!'
-        },
-        {
-            'author': {'nickname': 'Eric Yuan'},
-            'content': 'This is the second blog!'
-        },
-        {
-            'author': {'nickname': 'Eric Yuan'},
-            'content': 'This is the third blog!'
-        }
-    ]
-    return render_template("index.html",
-                           title='Home',
-                           user=user,
-                           blogs=blogs)
+    return render_template("index.html")
 
-@app.route('/login', methods=['GET','POST'])
-def login():
-    form = LoginForm()
-    if  form.validate_on_submit():
-        flash('Login requested for OpenID="%s", remember_me=%s' %
-              (form.openid.data, str(form.remember_me.data)))
-        return redirect('/index')
-    return render_template('login.html',
-                           title='Sign In',
-                           form=form)
+@app.route('/catalog')
+def catalog():
+    return render_template('catalog.html')
+
+@app.route('/comment')
+def comment():
+    return render_template('comment.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
